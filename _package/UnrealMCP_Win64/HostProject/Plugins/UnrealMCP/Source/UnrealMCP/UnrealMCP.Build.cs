3ae20164e27f3189e18827c7891877077a581abc
Seymi <seymi.hafsia@gmail.com>
2025-10-06 10:48:58 +0200

    fix(build): remove RenderingCommon, add RenderCore/RHI, validate dependencies for UE5.6 (#30)

--- _package/UnrealMCP_Win64/HostProject/Plugins/UnrealMCP/Source/UnrealMCP/UnrealMCP.Build.cs
+++ _package/UnrealMCP_Win64/HostProject/Plugins/UnrealMCP/Source/UnrealMCP/UnrealMCP.Build.cs
@@ -4,92 +4,95 @@ using UnrealBuildTool;
 
 public class UnrealMCP : ModuleRules
 {
-	public UnrealMCP(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		// Use IWYUSupport instead of the deprecated bEnforceIWYU in UE5.5
-		IWYUSupport = IWYUSupport.Full;
+    public UnrealMCP(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        // Use IWYUSupport instead of the deprecated bEnforceIWYU in UE5.5
+        IWYUSupport = IWYUSupport.Full;
 
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-		);
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-		);
-		
-                PublicDependencyModuleNames.AddRange(
-                        new string[]
-                        {
-                                "Core",
-                                "CoreUObject",
-                                "Engine",
-                                "InputCore",
-                                "Networking",
-                                "Sockets",
-                                "HTTP",
-                                "Json",
-                                "JsonUtilities",
-                                "DeveloperSettings"
-                        }
-                );
+        PublicIncludePaths.AddRange(
+            new string[]
+            {
+                // ... add public include paths required here ...
+            }
+        );
 
-                PrivateDependencyModuleNames.AddRange(
-                        new string[]
-                        {
-                                "UnrealEd",
-                                "Niagara",
-                                "NiagaraCore",
-                                "EditorScriptingUtilities",
-                                "EditorSubsystem",
-                                "Blutility",
-                                "Slate",
-                                "SlateCore",
-                                "UMG",
-                                "Kismet",
-                                "KismetCompiler",
-                                "BlueprintGraph",
-                                "Projects",
-                                "AssetRegistry",
-                                "AssetTools",
-                                "AudioMixer",
-                                "SignalProcessing",
-                                "RenderingCommon",
-                                "SkeletalMeshUtilitiesCommon",
-                                "SourceControl",
-                                "Settings",
-                                "LevelEditor",
-                                "MovieScene",
-                                "LevelSequence",
-                                "MovieSceneTracks",
-                                "SequencerScripting",
-                                "MovieSceneTools",
-                                "CinematicCamera"
-                        }
-                );
-		
-		if (Target.bBuildEditor == true)
-		{
-			PrivateDependencyModuleNames.AddRange(
-				new string[]
-				{
-					"PropertyEditor",      // For widget property editing
-					"ToolMenus",           // For editor UI
-					"BlueprintEditorLibrary", // For Blueprint utilities
-					"UMGEditor"           // For WidgetBlueprint.h and other UMG editor functionality
-				}
-			);
-		}
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-		);
-	}
-} 
+        PrivateIncludePaths.AddRange(
+            new string[]
+            {
+                // ... add other private include paths required here ...
+            }
+        );
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "CoreUObject",
+                "DeveloperSettings",
+                "Engine",
+                "HTTP",
+                "InputCore",
+                "Json",
+                "JsonUtilities",
+                "Networking",
+                "RenderCore",
+                "RHI",
+                "Sockets"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "AssetRegistry",
+                "AssetTools",
+                "AudioMixer",
+                "BlueprintGraph",
+                "Blutility",
+                "CinematicCamera",
+                "EditorScriptingUtilities",
+                "EditorSubsystem",
+                "Kismet",
+                "KismetCompiler",
+                "LevelEditor",
+                "LevelSequence",
+                "MovieScene",
+                "MovieSceneTools",
+                "MovieSceneTracks",
+                "Niagara",
+                "NiagaraCore",
+                "Projects",
+                "SequencerScripting",
+                "Settings",
+                "SignalProcessing",
+                "SkeletalMeshUtilitiesCommon",
+                "Slate",
+                "SlateCore",
+                "SourceControl",
+                "UMG",
+                "UnrealEd"
+            }
+        );
+
+        if (Target.bBuildEditor == true)
+        {
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "BlueprintEditorLibrary", // For Blueprint utilities
+                    "PropertyEditor",         // For widget property editing
+                    "ToolMenus",             // For editor UI
+                    "UMGEditor"               // For WidgetBlueprint.h and other UMG editor functionality
+                }
+            );
+        }
+
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
+                // ... add any modules that your module loads dynamically here ...
+            }
+        );
+    }
+}
